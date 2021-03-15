059bcd512d7eb208fa8716a9f8e6092124b3a4f1
不随 <1711865552@qq.com>
2021-03-15 16:21:28 +0800

    修改 App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let name;
+	let age = 12;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2>age = {age}</h2>
+	<p>Visit the to learn how to build Svelte apps.</p>
 </main>
 
 <style>

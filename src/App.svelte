37c9790ce5a1ea7c0b8ca26605301c2061e19584
不随 <1711865552@qq.com>
2021-03-15 17:14:47 +0800

    添加图片和webm 文件

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,19 @@
 <script>
 	export let name;
-	let age = 12;
+	let age = 1;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<h2>age = {age}</h2>
 	<p>Visit the to learn how to build Svelte apps.</p>
-	<video src="./static/video/webm.webm" />
+	<video src="../static/video/webm.webm" controls />
+	<div>
+		<img src="../static/images/OIP.jpg" alt="">
+	</div>
+	<p>
+		<img src="/a1.jpg" alt="">
+	</p>
 </main>
 
 <style>
@@ -17,7 +23,9 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
+	video{
+		max-width: 100%;
+	}
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
